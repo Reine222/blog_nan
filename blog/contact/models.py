b35da19b1c7a96ef4a317b5ef24bf13d13d78ca5
toure <lamagniguitoure@gmail.com>
2019-10-25 10:24:30 +0000

    creation de models
    creation de models  sans migration

--- blog/contact/models.py
+++ blog/contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    nom = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    sujet = models.CharField(max_length=50)
+    message = models.TextField()
+    date_add =models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nom
+
+class Newsletter(models.Model):
+    email = models.EmailField(max_length=50)
+    date_add =models.DateTimeField(auto_now_add=True)
+    date_up = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
