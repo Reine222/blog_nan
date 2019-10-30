22882305fa138211686ad2369db029cc2c63d877
Eunicepris <eunice.priscille@gmail.com>
2019-10-30 14:49:17 +0000

    ok

--- blog/administration/urls.py
+++ blog/administration/urls.py
@@ -3,12 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index_dash/', views.index_dash, name='index_dash'),
+    path('', views.index_dash, name='index_dash'),
+    path('admin_visiteur_dash/', views.admin_visiteur_dash, name='admin_visiteur_dash'),
+    path('detail_visiteur_dash', views.detail_visiteur_dash, name='detail_visiteur_dash'),
     path('page_dash/', views.page_dash, name='page_dash'),
     path('post_attend_dash/', views.post_attend_dash, name='post_attend_dash'),
+    path('post_partage_dash/', views.post_partage_dash, name='post_partage_dash'),
     path('post_valid_dash/', views.post_valid_dash, name='post_valid_dash'),
     path('form_aricle_dash/', views.form_aricle_dash, name='form_aricle_dash'),
     path('form_profil_dash/', views.form_profil_dash, name='form_profil_dash'),
     path('project_detail_dash/', views.project_detail_dash, name='project_detail_dash'),
+    path('profil_visiteur_dash/', views.profil_visiteur_dash, name='profil_visiteur_dash'),
     path('tables_dash/', views.tables_dash, name='tables_dash'),
+    path('tables_visiteur_dash/', views.tables_visiteur_dash, name='tables_visiteur_dash'),
 ]
--- blog/administration/views.py
+++ blog/administration/views.py
@@ -5,14 +5,26 @@ from django.shortcuts import render
 def index_dash(request):
     return render(request, 'pages/index_dash.html')
 
+def admin_visiteur_dash(request):
+    return render(request, 'pages/admin_visiteur_dash.html')
+
+def detail_visiteur_dash(request):
+    return render(request, 'pages/detail_visiteur_dash.html')
+
 def page_dash(request):
     return render(request, 'pages/page_dash.html')
 
 def post_attend_dash(request):
     return render(request, 'pages/post_attend_dash.html')
 
+def post_partage_dash(request):
+    return render(request, 'pages/post_partage_dash.html')
+
 def post_valid_dash(request):
     return render(request, 'pages/post_valid_dash.html')
+
+def profil_visiteur_dash(request):
+    return render(request, 'pages/profil_visiteur_dash.html')
 
 def form_aricle_dash(request):
     return render(request, 'pages/form_aricle_dash.html')
@@ -25,3 +37,6 @@ def project_detail_dash(request):
 
 def tables_dash(request):
     return render(request, 'pages/tables_dash.html')
+
+def tables_visiteur_dash(request):
+    return render(request, 'pages/tables_visiteur_dash.html')
