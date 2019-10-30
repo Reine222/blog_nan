03d25fa3d3907ff4cbd30372e36e135d0e6325b8
Gbane26 <medhijrgbanes@gmail.com>
2019-10-30 18:03:31 +0000

    vous m'avez enerver
    vous m'avez enerver

--- blog/administration/urls.py
+++ blog/administration/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index_dash, name='index_dash'),
+    path('tables_dash/', views.tables_dash, name='tables_dash'),
+    path('<int:pk>/project_detail_dash/', views.project_detail_dash, name='project_detail_dash'),
     path('admin_visiteur_dash/', views.admin_visiteur_dash, name='admin_visiteur_dash'),
     path('detail_visiteur_dash', views.detail_visiteur_dash, name='detail_visiteur_dash'),
     path('page_dash/', views.page_dash, name='page_dash'),
@@ -12,8 +14,8 @@ urlpatterns = [
     path('post_valid_dash/', views.post_valid_dash, name='post_valid_dash'),
     path('form_article_dash/', views.form_article_dash, name='form_article_dash'),
     path('form_profil_dash/', views.form_profil_dash, name='form_profil_dash'),
-    path('project_detail_dash/', views.project_detail_dash, name='project_detail_dash'),
+    
     path('profil_visiteur_dash/', views.profil_visiteur_dash, name='profil_visiteur_dash'),
-    path('tables_dash/', views.tables_dash, name='tables_dash'),
+    
     path('tables_visiteur_dash/', views.tables_visiteur_dash, name='tables_visiteur_dash'),
 ]
